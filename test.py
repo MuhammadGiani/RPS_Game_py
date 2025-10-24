435660e2f1107bcf3b8c054060ecacad53506030
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2025-10-24 22:12:28 +0500

    Completed RPS game with functions, nested if,elif statements

--- test.py
+++ test.py
@@ -8,10 +8,35 @@ def get_choices():
     return choices
 
 def check_win(player, computer):
+
     print(f"You chose {player}, computer chose {computer}")
+
     if player==computer:
         return "It's a tie"
+    
+    elif player=="rock":
+        if computer=="scissors":
+            return "Rock smashes scissors, you win!"
+        else:
+            return "Paper covers rock! Computer Wins."
 
-check_win("rock","scissor")
+    elif player=="paper":
+        if computer=="rock":
+            return "Paper covers rock, you win!"
+        else:
+            return "Scissors cuts paper, Computer Wins."
+    
+    elif player=="scissors":
+        if computer=="paper":
+            return "Scissors cuts paper, you win!"
+        else:
+            return "Rock smashes scissors! Computer Wins."
+   
+choices = get_choices()
 
-print()
+result = check_win(choices["player"], choices["computer"])
+print(result)
+
+# alpha = {"name":"ahmed", "age":"13"}
+# name = alpha["age"]
+# print(name)
