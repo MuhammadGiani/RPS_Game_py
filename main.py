50980b2aa67416231182fe76585b633044c0271a
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2025-10-26 21:46:21 +0500

    Practice string functions, ternary operator, Operators

---
+++ main.py
@@ -0,0 +1,43 @@
+name = "Jack"
+age = 15
+income = str(256.78)
+
+# print(isinstance(name,str))
+# print(isinstance(age,str))
+# print(isinstance(income,str))
+
+# c1=True
+# c2=False
+
+# print("Conditions")
+# print(c1 and c2)
+# print(c1 or c2)
+# print(not c1)
+# print(not c2)
+
+#Ternary operator
+# def IsAdult(age):
+#     return True if age>18 else False
+
+# print(IsAdult(age))
+
+#Multi line string
+# print("""This is 
+      
+#       a multi 
+
+#       line String
+
+# thank you
+# """)
+
+print("\"Muhammad giani\" is my name  ")
+
+# python data types
+complex
+dict
+list
+tuple
+set
+range
+bool
